99df036a06e69de6d8092a0ded411784286bd894
Matheus Murilo da Silveira <matheusmurilo097@gmail.com>
2025-02-10 14:26:02 +0000

    Edição do arquivo

--- TrunfoMMS.c
+++ TrunfoMMS.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+// Desafio Super Tunfo - Países
+// Tema 1 - Cadastro das cartas
+// Objetivo: No nível novato você deve criar as cartas representando as cidades utulizando
+
 int main(){
-    printf("Super Trunfo Países!\n");
-    printf("Novo commit\n");
+    // Área para definição das variáveis para armazenar as propriedades das cidades
+    int população;
+
+    // Área para entrada de dados
+    scanf("%d", &população);
+
+    // Área para exibição dos dados da cidade
+    printf("Exibição dados das cidades");
+    return 0;
 }
